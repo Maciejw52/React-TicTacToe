63a1c4f9db12463961ef390b98ca0289d0192ab4
Wyszogrodzki <maciek.wyszogrodzki@dematic.com>
2022-05-30 13:38:14 +0100

    changed the gameboard styling

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -46,15 +46,15 @@ export default function GameBoard({ tileSymbols, gameSymbol, setGameSymbol }) {
     <TurnTracker/>
 
     <div className="gridContainer">
-      <div className="gridItem" onClick={ () => {playerClick(1)}} >{gameSymbol[1]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(2)}} >{gameSymbol[2]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(3)}} >{gameSymbol[3]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(4)}} >{gameSymbol[4]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(5)}} >{gameSymbol[5]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(6)}} >{gameSymbol[6]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(7)}} >{gameSymbol[7]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(8)}} >{gameSymbol[8]}</div>
-      <div className="gridItem" onClick={ () => {playerClick(9)}} >{gameSymbol[9]}</div>
+      <div className="gridItem12" onClick={ () => {playerClick(1)}} >{gameSymbol[1]}</div>
+      <div className="gridItem12" onClick={ () => {playerClick(2)}} >{gameSymbol[2]}</div>
+      <div className="gridItem3" onClick={ () => {playerClick(3)}} >{gameSymbol[3]}</div>
+      <div className="gridItem1" onClick={ () => {playerClick(4)}} >{gameSymbol[4]}</div>
+      <div className="gridItem1" onClick={ () => {playerClick(5)}} >{gameSymbol[5]}</div>
+      <div className="gridItem69" onClick={ () => {playerClick(6)}} >{gameSymbol[6]}</div>
+      <div className="gridItem1" onClick={ () => {playerClick(7)}} >{gameSymbol[7]}</div>
+      <div className="gridItem1" onClick={ () => {playerClick(8)}} >{gameSymbol[8]}</div>
+      <div className="gridItem69" onClick={ () => {playerClick(9)}} >{gameSymbol[9]}</div>
     </div>
 
     <Winner/>
